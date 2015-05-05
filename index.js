4d5a20b41102e15dc804545e59581fb77738d2db
Daniel Cousens <github@dcousens.com>
2015-05-05 15:55:11 +1000

    adhere to standard

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-function squared(x) { return x * x }
-function toRad(x) { return x * Math.PI / 180 }
+function squared (x) { return x * x }
+function toRad (x) { return x * Math.PI / 180 }
 
 var atan2 = Math.atan2
 var cos = Math.cos
@@ -10,12 +10,12 @@ var sqrt = Math.sqrt
 var R = 6378137
 
 // https://stackoverflow.com/questions/1502590/calculate-distance-between-two-points-in-google-maps-v3
-module.exports = function haversineDistance(a, b) {
+module.exports = function haversineDistance (a, b) {
   var dLat = toRad(b.latitude - a.latitude)
   var dLon = toRad(b.longitude - a.longitude)
 
   var f = squared(sin(dLat / 2)) + cos(toRad(a.latitude)) * cos(toRad(b.latitude)) * squared(sin(dLon / 2))
-	var c = 2 * atan2(sqrt(f), sqrt(1 - f))
+  var c = 2 * atan2(sqrt(f), sqrt(1 - f))
 
   return R * c
 }
--- test/index.js
+++ test/index.js
@@ -4,9 +4,9 @@ var assert = require('assert')
 var haversine = require('../')
 var fixtures = require('./fixtures')
 
-describe('haversine', function() {
-  fixtures.forEach(function(f) {
-    it('returns ' + f.expected + ' for ' + f.arguments, function() {
+describe('haversine', function () {
+  fixtures.forEach(function (f) {
+    it('returns ' + f.expected + ' for ' + f.arguments, function () {
       var actual = haversine.apply(null, f.arguments)
 
       assert.equal(actual, f.expected)
