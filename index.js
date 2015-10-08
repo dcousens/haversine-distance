1a0a8509a6a8c7ad8a5eccf6d7c8391b413c1c27
Daniel Cousens <github@dcousens.com>
2015-10-08 13:14:53 +1100

    use const, asm.js for floats

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
+const atan2 = Math.atan2
+const cos = Math.cos
+const sin = Math.sin
+const sqrt = Math.sqrt
+const PI = Math.PI
+
+ // (mean) radius of Earth (meters)
+const R = 6378137
+
 function squared (x) { return x * x }
-function toRad (x) { return x * Math.PI / 180 }
-
-var atan2 = Math.atan2
-var cos = Math.cos
-var sin = Math.sin
-var sqrt = Math.sqrt
-
-// (mean) radius of Earth (meters)
-var R = 6378137
+function toRad (x) { return x * PI / 180.0 }
 
 // https://stackoverflow.com/questions/1502590/calculate-distance-between-two-points-in-google-maps-v3
 module.exports = function haversineDistance (a, b) {
@@ -19,8 +20,9 @@ module.exports = function haversineDistance (a, b) {
   var dLat = toRad(bLat - aLat)
   var dLon = toRad(bLng - aLng)
 
-  var f = squared(sin(dLat / 2)) + cos(toRad(aLat)) * cos(toRad(bLat)) * squared(sin(dLon / 2))
+  var f = squared(sin(dLat / 2.0)) + cos(toRad(aLat)) * cos(toRad(bLat)) * squared(sin(dLon / 2.0))
   var c = 2 * atan2(sqrt(f), sqrt(1 - f))
 
   return R * c
 }
+
