55b0e2f92bea3acea85564b138174426ee40f39c
Daniel Cousens <dcousens@users.noreply.github.com>
2019-10-16 10:09:40 +1100

    add some more tests

--- test/index.js
+++ test/index.js
@@ -3,10 +3,10 @@ const fixtures = require('./fixtures')
 const tape = require('tape')
 
 fixtures.forEach(function (f) {
-  tape('returns ' + f.expected + ' for ' + JSON.stringify(f.arguments), function (t) {
+  tape('returns ' + f.expected + ' for ' + f.description || JSON.stringify(f.arguments), function (t) {
     const actual = haversine.apply(null, f.arguments)
 
     t.plan(1)
-    t.ok(Math.abs(actual - f.expected) < 0.0001, `${actual} ~= ${f.expected}`)
+    t.ok(Math.round(actual) === f.expected, `${actual} ~= ${f.expected}`)
   })
 })
